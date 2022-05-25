273e7c19b6f37e909b2a28a3188b3c01d9543152
oliverritchie <oritchie77@gmail.com>
2022-05-25 16:40:56 +0100

    button styling done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.scss';
+import CircleButton from "./CircleButton/CircleButton";
 
 function App() {
   return (
     <div className="App">
-
+      <CircleButton
+        svgType={'expand'}
+      />
+        <CircleButton
+            svgType={'closeButton'}
+        />
     </div>
   );
 }
